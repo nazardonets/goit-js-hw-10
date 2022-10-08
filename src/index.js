7131aefdbdc4ca40d772a8bd17aba6c3907228b9
Nazar D <nazardonets@gmail.com>
2022-10-08 22:43:46 +0300

    finished task

--- src/index.js
+++ src/index.js
@@ -1,56 +1,98 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+import countryDetailsTmp from './templates/countryDetails.hbs';
+import countriesListTmp from './templates/countriesList.hbs';
 
 const BASE_URL = 'https://restcountries.com/v3.1';
+const OPTIONS = 'fields=name,capital,population,flags,languages';
 const DEBOUNCE_DELAY = 300;
 
 const searchInputEl = document.querySelector('#search-box');
 const countryContainerEl = document.querySelector('.country');
+const countriesListEl = document.querySelector('.country-list');
 
 function fetchCountriesByName(name) {
-   fetch(`${BASE_URL}/name/${name}`)
-      .then(r => {
-         return r.json();
-      })
-      .then(data => {
-         console.log(data);
-         renderTemplate(data[0]);
-         return data;
+   return fetch(`${BASE_URL}/name/${name}?${OPTIONS}`)
+      .then(response => {
+         if (response.status === 404) {
+            throw new Error('grebanniy "fetch" ne lovit oshibku normalno');
+         }
+         return response.json();
       })
       .catch(error => console.log(error));
 }
 
-searchInputEl.addEventListener(
-   'input',
-   debounce(e => {
-      if (e.target.value === '') {
-         return (countryContainerEl.innerHTML = '');
-      }
-      let countryName = e.target.value;
-      fetchCountriesByName(countryName);
-   }, DEBOUNCE_DELAY)
-);
+function onInputEvent(e) {
+   let countryName = removeSpacesFromInput(e.target.value);
 
-function renderTemplate(country) {
-   let template = `<h1 class='country-title'>
-            <img class='country-flag' src='${
-               country.flags.png
-            }' height="25" width="auto"/>
-            <span data-country-name>${country.name.official}</span>
-            </h1>
-            <ul class='country-info__list'>
-               <li class='country-info__item'>
-                  <b>Capital:</b> ${country.name.common}
-               </li>
-               <li class='country-info__item'>
-                  <b>Population:</b> ${country.population}
-               </li>
-               <li class='country-info__item'>
-                  <b>Languages:</b> ${Object.values(country.languages).join(
-                     ', '
-                  )}
-               </li>
-            </ul>`;
+   if (countryName === '') {
+      return resetMarkup();
+   }
+
+   return renderUI(countryName);
+}
+
+function renderUI(countryName) {
+   fetchCountriesByName(countryName)
+      .then(jsonData => {
+         if (jsonData.length === 1) {
+            resetMarkup();
+            return createCountryDetailsMarkup(jsonData[0]);
+         }
+         if (jsonData.length > 1 && jsonData.length <= 9) {
+            resetMarkup();
+            return createCountriesListMarkup(jsonData);
+         }
+
+         if (jsonData.length > 10) {
+            return Notify.info(
+               'Too many matches found. Please enter a more specific name.'
+            );
+         }
+      })
+      .catch(() => {
+         Notify.failure(`Oops, there is no country with name "${countryName}"`);
+         resetMarkup();
+      });
+}
+
+function createCountryDetailsMarkup(countryName) {
+   let template = countryDetailsTmp(countryName);
 
    return (countryContainerEl.innerHTML = template);
 }
+
+function createCountriesListMarkup(countries) {
+   let markup = '';
+   let iteration = 1;
+
+   countries.forEach(country => {
+      if (iteration > 5) {
+         return;
+      }
+
+      markup += countriesListTmp(country);
+
+      iteration += 1;
+
+      return markup;
+   });
+
+   return (countriesListEl.innerHTML = markup);
+}
+
+function resetMarkup() {
+   countryContainerEl.innerHTML = '';
+   countriesListEl.innerHTML = '';
+}
+
+function removeSpacesFromInput(value) {
+   return value.trim();
+}
+
+searchInputEl.addEventListener(
+   'input',
+   debounce(e => onInputEvent(e), DEBOUNCE_DELAY)
+);
