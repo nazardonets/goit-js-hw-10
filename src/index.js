d1876e3c1d0a6b9e4c46b997a3a95d630d3cd155
Nazar D <nazardonets@gmail.com>
2022-10-09 00:43:36 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
-import { fetchCountriesByName } from './fetchCountries.js';
+import { fetchCountriesByName } from './fetchCountries';
 import {
    createCountryDetailsMarkup,
    createCountriesListMarkup,
